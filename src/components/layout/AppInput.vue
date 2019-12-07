e23f7956f9a230eb466b3cce3ae31ae621acb4a1
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-12-07 13:06:07 +0200

    fixing if coondition logic

--- src/components/layout/AppInput.vue
+++ src/components/layout/AppInput.vue
@@ -3,11 +3,22 @@
     <label
       class="control-label"
       for="inputWarning"
-      v-if="inputWarnings['has-error'] && !notInteger"
+      v-if="
+        inputWarnings['has-error'] &&
+          !notRightNum &&
+          !notBuyableNum &&
+          !notSellableNum
+      "
       >text is for bidden</label
     >
-    <label class="control-label" for="inputWarning" v-else-if="notInteger"
-      >float numbers is forbidden</label
+    <label class="control-label" for="inputWarning" v-else-if="notRightNum"
+      >logical number is required</label
+    >
+    <label class="control-label" for="inputWarning" v-else-if="notBuyableNum"
+      >you do not have enough funds</label
+    >
+    <label class="control-label" for="inputWarning" v-else-if="notSellableNum"
+      >you do not have enough stocks</label
     >
     <label class="control-label" for="inputWarning" v-else
       >enter the quantity</label
@@ -18,18 +29,31 @@
 <script lang="ts">
 import Vue from "vue";
 import { Component, Prop, Watch, Emit } from "vue-property-decorator";
-import { ITypeWarning } from "@/types";
+import { ITypeWarning, IModePayload } from "@/types";
+import { Getter } from "vuex-class";
+
+const namespace = "profile";
+
 @Component
 export default class AppInput extends Vue {
   @Prop({
     required: true
   })
-  value: string;
+  value: string | number;
+
+  @Prop({
+    required: false
+  })
+  mode: IModePayload;
+
+  @Getter("getFunds", { namespace }) getFunds: number;
 
   initialData = this.value;
 
-  notInteger: boolean = false;
+  notRightNum: boolean = false;
 
+  notBuyableNum: boolean = false;
+  notSellableNum: boolean = false;
   @Emit()
   input(value: number) {
     return value;
@@ -40,29 +64,66 @@ export default class AppInput extends Vue {
     return inputWarning;
   }
 
+  @Watch("value")
+  onValue(newValue: number, oldQuantity: number) {
+    this.initialData = newValue;
+  }
+
   inputWarnings: ITypeWarning = { "has-error": false, "has-success": false };
   @Watch("initialData")
-  onValue(newQuantity: string, oldQuantity: string) {
-    if (isNaN(Number(newQuantity))) {
-      this.inputWarnings["has-error"] = true;
-      this.inputWarnings["has-success"] = false;
-      this.notInteger = false;
-    } else if (Number.isInteger(Number(newQuantity))) {
-      this.inputWarnings["has-error"] = false;
-      this.inputWarnings["has-success"] = true;
-      this.notInteger = false;
-      this.input(Number(newQuantity));
-    } else {
-      this.inputWarnings["has-error"] = true;
-      this.inputWarnings["has-success"] = false;
-      this.notInteger = true;
+  onInitialData(newQuantity: string, oldQuantity: string) {
+    const res = this.checkInput(newQuantity);
+    if (res) {
+      this.input(parseInt(newQuantity));
     }
-    if (newQuantity.length == 0) {
-      this.inputWarnings["has-error"] = false;
-      this.inputWarnings["has-success"] = false;
-    }
+  }
 
+  checkBuyingStocks(quantity: number, stockPrice: number): boolean {
+    const fund = this.getFunds;
+    if (fund === 0) {
+      return false;
+    } else if (fund < quantity * stockPrice) {
+      return false;
+    }
+    return true;
+  }
+
+  checkSellingStocks(oldQuantity: number, newQuantity: number): boolean {
+    console.log(newQuantity, oldQuantity);
+    if (newQuantity > oldQuantity) {
+      return false;
+    }
+    return true;
+  }
+
+  checkInput(newQuantity: string): boolean {
+    this.inputWarnings["has-error"] = false;
+    this.inputWarnings["has-success"] = false;
+    this.notRightNum = false;
+    this.notBuyableNum = false;
+    this.notSellableNum = false;
+    const num = Number(newQuantity);
+
+    if (isNaN(num)) {
+      this.inputWarnings["has-error"] = true;
+    } else if (!Number.isInteger(num) || num <= 0) {
+      this.inputWarnings["has-error"] = true;
+      this.notRightNum = true;
+    } else if (this.mode.mode == "buy") {
+      const res = this.checkBuyingStocks(num, this.mode.stockPrice as number);
+      this.notBuyableNum = this.inputWarnings["has-error"] = !res;
+      this.inputWarnings["has-success"] = res;
+      this.warningClass(this.inputWarnings);
+      return res;
+    } else if (this.mode.mode == "sell") {
+      const res = this.checkSellingStocks(this.mode.oldQuantity as number, num);
+      this.notSellableNum = this.inputWarnings["has-error"] = !res;
+      this.inputWarnings["has-success"] = res;
+      this.warningClass(this.inputWarnings);
+      return res;
+    }
     this.warningClass(this.inputWarnings);
+    return false;
   }
 }
 </script>

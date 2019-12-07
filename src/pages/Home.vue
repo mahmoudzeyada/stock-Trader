b17e2af66d3198493bf8ba2092221b7bbf986fbc
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-12-07 13:07:49 +0200

    creating home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="row">
     <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-      <h1>Home</h1>
+      <h1>Trade or View your Portfolio</h1>
+      <h6>You may Save & Load your Data</h6>
+      <h6>Click on 'End Day' to begin a New Day!</h6>
+      <hr />
+      <p>Your Funds: {{ funds }}</p>
     </div>
   </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
-export default Vue.extend({});
+export default Vue.extend({
+  computed: {
+    funds() {
+      return this.$store.getters["profile/getFunds"];
+    }
+  }
+});
 </script>

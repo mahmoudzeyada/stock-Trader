6bb64cfa1a22c88d50ff0e3c6c9268b09c8986ff
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-12-08 12:30:19 +0200

    adding filters to comp

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import Vue from "vue";
 
 import App from "./App.vue";
 import router from "./router";
+import store from "./store";
 
 Vue.config.productionTip = false;
 
+Vue.filter("currency", (value: number) => {
+  return "$" + value.toLocaleString();
+});
 new Vue({
   render: h => h(App),
-  router
+  router,
+  store
 }).$mount("#app");

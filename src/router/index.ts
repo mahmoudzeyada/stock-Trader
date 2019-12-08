3ad7128e8c321ee59ddecb25d6f6263cd3ed7ad9
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-12-08 12:25:42 +0200

    adding the remaing routers to home comp

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,14 +3,15 @@ import VueRouter from "vue-router";
 
 import Home from "../pages/Home.vue";
 import Stock from "../pages/StockPage/index.vue";
-import Portfolio from "../pages/Portfolio.vue";
+import Portfolio from "../pages/Portfolio/index.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: "/", component: Home },
   { path: "/stock", component: Stock },
-  { path: "/portfolio", component: Portfolio }
+  { path: "/portfolio", component: Portfolio },
+  { path: "*", component: Home }
 ];
 
 const router = new VueRouter({ routes, mode: "history" });
